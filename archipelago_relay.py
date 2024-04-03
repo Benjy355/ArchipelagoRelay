cfd9b1d39a10746ecdb067419df7858e5bcc8323
Benjy355 <benjy355@gmail.com>
2024-04-02 22:00:12 -0700

    Fixed retrieving item/names
    Don't copy paste your own code, kids.

--- archipelago_relay.py
+++ archipelago_relay.py
@@ -55,30 +55,30 @@ class archi_relay:
         return self._multiworld_site_data.players[0]
     
     async def _get_playerName_by_id(self, id: int):
-        for player in self.network_players:
+        for player in self._archi_players:
             if player.slot == id:
                 return player.name
             
     async def _get_playerAlias_by_id(self, id: int):
-        for player in self.network_players:
+        for player in self._archi_players:
             if player.slot == id:
                 return player.alias
             
     async def _get_playerGame_by_id(self, id: int):
         slotName = ""
         pName = await self._get_playerName_by_id(id)
-        for slot in self.network_slots:
+        for slot in self._archi_slot_info:
             if slot.name == pName:
                 return slot.game
             
     async def _get_itemName_by_id(self, id: int, playerId: int):
         game = await self._get_playerGame_by_id(playerId)
-        item_name = self.game_data[game]['item_id_to_name'][int(id)]
+        item_name = game_cache.get_game_cache(game)['item_id_to_name'][int(id)]
         return item_name
     
     async def _get_locationName_by_id(self, id: int, playerId: int):
         game = await self._get_playerGame_by_id(playerId)
-        loc_name = self.game_data[game]['location_id_to_name'][int(id)]
+        loc_name = game_cache.get_game_cache(game)['location_id_to_name'][int(id)]
         return loc_name
     
     def get_archi_game_version(self, game:str) -> int:
